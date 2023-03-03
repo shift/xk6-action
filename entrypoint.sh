1312a0dd001329c10e2f8d292696fabd135a3c72
Vincent Palmer <shift@someone.section.me>
2023-03-03 12:45:29 +0100

    chore: use locally built k6 from xk6

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 # or splits `args` in a string separated by whitespace.
 export K6_CLOUD_TOKEN=$INPUT_TOKEN
 export K6_COMMAND=$([[ $INPUT_CLOUD == "true" ]] && echo 'cloud' || echo 'run')
-sh -c "k6 $K6_COMMAND $INPUT_FILENAME $INPUT_FLAGS"
+sh -c "./k6 $K6_COMMAND $INPUT_FILENAME $INPUT_FLAGS"
